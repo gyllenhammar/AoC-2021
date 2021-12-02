f1d4c250fe685ce272b7c067f0fa4a4153bff16e
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2021-12-02 23:41:04 +0100

    Day 1 solution

---
+++ day1/sonarsweep.py
@@ -0,0 +1,35 @@
+
+def solveProblem1(numbers):
+  num_increase = 0
+  prev_number = 0
+
+  for number in numbers:
+      if int(number) > prev_number:
+          num_increase = num_increase + 1
+      prev_number = int(number)
+
+  print(f"problem 1 is {num_increase - 1}")
+
+def solveProblem2(numbers):
+  num_increase = 0
+  
+  for i in range(2, len(numbers) -1):
+      if (numbers[i - 2] + numbers[i - 1] + numbers[i]) < (numbers[i-1] + numbers[i] + numbers[i + 1]):
+          num_increase += 1
+  
+  print(f"problem 2 is {num_increase}")
+
+
+if __name__ == '__main__':
+    try:
+        with open("input.txt", "r") as file:
+            data = [int(line.strip()) for line in file]
+    except IOError:
+        print("Error: Can't open file")
+        import sys
+        sys.exit(0)
+    
+    solveProblem1(data)
+    solveProblem2(data)
+
+    file.close()
