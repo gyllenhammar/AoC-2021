320755c2bde57da5a2466e62988c477332379459
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2021-12-02 23:57:07 +0100

    refactoring

--- day2/dive.py
+++ day2/dive.py
@@ -1,32 +1,34 @@
 def solveProblem1(instructions):
-    coordinates = [0, 0]
+    horizontal = 0
+    depth = 0
     for instruction in instructions:
-        instruction = instruction.split(" ")
-        if (instruction[0] == "up"):
-            coordinates[1] -= int(instruction[1])
-        elif (instruction[0] == "down"):
-            coordinates[1] += int(instruction[1])
-        elif (instruction[0] == "forward"):
-            coordinates[0] += int(instruction[1])
+        action = instruction.split(" ")[0]
+        value = int(instruction.split(" ")[1])
+        if (action == "up"):
+            depth -= value
+        elif (action == "down"):
+            depth += value
+        elif (action == "forward"):
+            horizontal += value
 
-    print(f"problem 2 is {coordinates[0] * coordinates[1]}")
+    print(f"problem 2 is {depth * horizontal}")
 
 def solveProblem2(instructions):
-    coordinates = [0, 0]
+    horizontal = 0
+    depth = 0
     aim = 0
     for instruction in instructions:
-        instruction = instruction.split(" ")
-        if (instruction[0] == "up"):
-            #coordinates[1] -= int(instruction[1])
-            aim -= int(instruction[1])
-        elif (instruction[0] == "down"):
-            #coordinates[1] += int(instruction[1])
-            aim += int(instruction[1])
-        elif (instruction[0] == "forward"):
-            coordinates[0] += int(instruction[1])
-            coordinates[1] += aim * int(instruction[1])
+        action = instruction.split(" ")[0]
+        value = int(instruction.split(" ")[1])
+        if (action == "up"):
+            aim -= value
+        elif (action == "down"):
+            aim += value
+        elif (action == "forward"):
+            horizontal += value
+            depth += aim * value
     
-    print(f"problem 2 is {coordinates[0] * coordinates[1]}")
+    print(f"problem 2 is {depth * horizontal}")
 
 if __name__ == '__main__':
     try:
